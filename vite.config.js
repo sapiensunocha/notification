a2eaf55caffc1100be28697d1e4c64f0323434a5
Your Name <your.email@example.com>
2025-07-09 23:54:56 -0400

    Add serve package for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './', // <--- THIS LINE MUST BE PRESENT AND CORRECT
 })
